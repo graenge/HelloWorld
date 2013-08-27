a981d92ec9fbe3265d9c5996e8312871e0ffb318
Thomas Grænge <Thomas@Thomass-MacBook-Pro.local>
2013-08-27 09:20:54 +0200

    Initial Commit

---
+++ Hello World/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Thomas Grænge on 27/08/13.
+//  Copyright (c) 2013 Thomas Grænge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Hello World/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Thomas Grænge on 27/08/13.
+//  Copyright (c) 2013 Thomas Grænge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
